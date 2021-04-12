3a337f52d8b085ef9c9c4bd2a0802b368c913a3b
Kehinde Akande <Longlasting1805>
2021-04-12 19:43:04 +0800

    chapter5Exercises

---
+++ src/Chapter5Exercises/ProductOfOdd.java
@@ -0,0 +1,15 @@
+package Chapter5Exercises;
+
+public class ProductOfOdd {
+    public
+    static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 15; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+        System.out.printf("sumProduct of odd is %d%n", sum);
+
+    }
+}

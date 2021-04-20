8e6cc4525e092f84518d693a582c12b2292b1095
Kehinde Akande <Longlasting1805>
2021-04-20 21:49:43 +0800

    RemoveContinue

---
+++ src/Chapter5Exercises/RemoveContinue.java
@@ -0,0 +1,9 @@
+package Chapter5Exercises;
+
+public class RemoveContinue {
+    public static void main(String[] args) {
+        for (int count = 1; count <= 10; count++){
+            System.out.printf("%d",count);
+        }
+    }
+}

46114496b2c4be559dd2e4c739c935e30aa0afa5
Kehinde Akande <Longlasting1805>
2021-04-20 20:29:26 +0800

    Critism

---
+++ src/Chapter5Exercises/Criticism.java
@@ -0,0 +1,11 @@
+package Chapter5Exercises;
+
+public class Criticism {
+    public static void main(String[] args) {
+        int count;
+
+        for (count = 1; count <= 5; count++){
+            System.out.printf("%d",count);
+        }
+    }
+}

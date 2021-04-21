bb0c18feabd80ee79268c5d300bc59f801c16779
Kehinde Akande <Longlasting1805>
2021-04-21 20:45:22 +0800

    TheTwelveDayOfChristmasDaySong

--- src/Chapter5Exercises/CalculatingSales.java
+++ src/Chapter5Exercises/CalculatingSales.java
@@ -14,10 +14,10 @@ public class CalculatingSales {
         quantity = input.nextInt();
         while (productNumber != -1){
             switch (productNumber) {
-                case 1 -> total+= 2.98 * quantity;
-                case 2 -> total+= 4.50 * quantity;
-                case 3 -> total+= 9.98 * quantity;
-                case 4 -> total+= 4.49 * quantity;
+                case 1 -> total += 2.98 * quantity;
+                case 2 -> total += 4.50 * quantity;
+                case 3 -> total += 9.98 * quantity;
+                case 4 -> total += 4.49 * quantity;
                 case 5 -> total += 6.87 * quantity;
             }
             System.out.println("enter product number & -1 to quit");

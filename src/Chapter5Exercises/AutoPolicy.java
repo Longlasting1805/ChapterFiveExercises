aee899cb3cd59f0d18b8b12f5bc034e82bfcd1f3
Kehinde Akande <Longlasting1805>
2021-04-22 02:06:23 +0800

    AutoPolicy

---
+++ src/Chapter5Exercises/AutoPolicy.java
@@ -0,0 +1,58 @@
+package Chapter5Exercises;
+
+public class AutoPolicy {
+    public static void main(String[] args) {
+
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getMakeAndModel() {
+        return makeAndModel;
+    }
+
+    public void setMakeAndModel(String makeAndModel) {
+        this.makeAndModel = makeAndModel;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        if ((state.equals("CT") ||state.equals("MA") ||state.equals("ME")  ||state.equals("NH")  ||state.equals("NJ")  ||state.equals("NY")  ||state.equals("PA")  ||state.equals("VT") )){
+            state = state;
+        }else{
+            System.out.println("the state code is wrong");
+        }
+    }
+
+    private int accountNumber;
+    private String makeAndModel;
+    private String state;
+
+    public AutoPolicy(int accountNumber, String makeAndModel, String state){
+        this.accountNumber = accountNumber;
+        this.makeAndModel = makeAndModel;
+        this.state = state;
+    }
+    public boolean isNoFaultState(){
+
+            boolean noFaultState = switch (state) {
+                case "CT", "MA", "ME", "NH", "NJ", "NY", "PA", "VT" -> true;
+                default -> false;
+            };
+        return noFaultState;
+
+    }
+
+
+
+
+}
---
+++ src/Chapter5Exercises/AutoPolicyObject.java
@@ -0,0 +1,35 @@
+package Chapter5Exercises;
+
+import java.io.Console;
+import java.security.Policy;
+
+public class AutoPolicyObject {
+    public static void main(String[] args) {
+        AutoPolicy policy1 = new AutoPolicy(11111111, "Acura ZDX", "CT");
+        AutoPolicy policy2 = new AutoPolicy(2222222, "Lexus", "MA");
+        AutoPolicy policy3 = new AutoPolicy(3333333, "ferrari", "ME");
+        AutoPolicy policy4 = new AutoPolicy(4444444, "Royce Rolls", "NH");
+        AutoPolicy policy5 = new AutoPolicy(5555555, "BMW", "NJ");
+        AutoPolicy policy6 = new AutoPolicy(6666666, "Porche", "NY");
+        AutoPolicy policy7 = new AutoPolicy(7777777, "Bently", "PA");
+        AutoPolicy policy8 = new AutoPolicy(6666666, "Lamboghini", "VT");
+
+        PolicyInNoFaultState(policy1);
+        PolicyInNoFaultState(policy2);
+        PolicyInNoFaultState(policy3);
+        PolicyInNoFaultState(policy4);
+        PolicyInNoFaultState(policy5);
+        PolicyInNoFaultState(policy6);
+        PolicyInNoFaultState(policy7);
+        PolicyInNoFaultState(policy8);
+    }
+
+    public static void PolicyInNoFaultState(AutoPolicy policy) {
+        System.out.println("The Auto Policy:");
+        System.out.printf("Account#:%d; Car: %s%n State %s %s%n A No Fault State %n%n", policy.getAccountNumber(), policy.getMakeAndModel(), policy.getState(),
+                (policy.isNoFaultState() ? "is" : "is not"));
+
+    }
+
+}
+
